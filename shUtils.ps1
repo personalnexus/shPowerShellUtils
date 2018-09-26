3617bc6d531ec268fc70f0b0d3268d856fea4485
StefanH <personalnexus@users.noreply.github.com>
2018-09-26 21:22:38 +0200

    added function to sort track points in a GPX file

--- shUtils.ps1
+++ shUtils.ps1
@@ -17,3 +17,15 @@ Function Set-UdpExemptPortRange($udpExemptPortRange)
 
     New-ItemProperty HKLM:\System\CurrentControlSet\services\Tcpip\Parameters\ -Name UdpExemptPortRange -Value $udpExemptPortRange -PropertyType MultiString -Force
 }
+
+
+Function Sort-GpxFile($inputFileName, $outputFileName)
+{
+    # See also: https://personalnexus.wordpress.com/2018/09/26/the-case-of-lightroom-placing-all-photos-at-the-end-of-the-track-sorting-a-gpx-file/
+
+    [xml]$xml = Get-Content $inputFileName
+    ($points = ($xml.gpx.trk.trkseg.trkpt | Sort-Object -Property time -Descending) ) | Out-Null
+    $firstPoint = $points[-1]
+    $points | ForEach-Object { $xml.gpx.trk.trkseg.InsertAfter($_, $firstPoint) } | Out-Null
+    $xml.Save($outputFileName)
+}
