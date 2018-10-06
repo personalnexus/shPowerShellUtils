838343fd3e06231c4e0713737514b43d6c74792a
StefanH <personalnexus@users.noreply.github.com>
2018-10-06 10:03:46 +0200

    added conversion between SIDs and usernames

--- shUtils.ps1
+++ shUtils.ps1
@@ -1,12 +1,12 @@
-﻿Function Show-NicTeamProperties($nicTeamName)
+Function Show-NicTeamProperties($NicTeamName)
 {
     # See also: https://personalnexus.wordpress.com/2018/04/29/the-case-of-multicast-message-loss-again/
 
-    (Get-NetLbfoTeam $nicTeamName).Members | Get-NetAdapterAdvancedProperty | ft DisplayName,DisplayValue,ValidDisplayValues,NumericParameterMaxValue
+    (Get-NetLbfoTeam $NicTeamName).Members | Get-NetAdapterAdvancedProperty | ft DisplayName,DisplayValue,ValidDisplayValues,NumericParameterMaxValue
 }
 
 
-Function Set-UdpExemptPortRange($udpExemptPortRange)
+Function Set-UdpExemptPortRange($UdpExemptPortRange)
 {
     # Example usage with nonconsecutive ranges:
     #
@@ -15,17 +15,62 @@ Function Set-UdpExemptPortRange($udpExemptPortRange)
     #
     # See also: https://personalnexus.wordpress.com/2017/02/06/the-case-of-multicast-message-loss-on-windows-server-2012-r2/
 
-    New-ItemProperty HKLM:\System\CurrentControlSet\services\Tcpip\Parameters\ -Name UdpExemptPortRange -Value $udpExemptPortRange -PropertyType MultiString -Force
+    New-ItemProperty HKLM:\System\CurrentControlSet\services\Tcpip\Parameters\ -Name UdpExemptPortRange -Value $UdpExemptPortRange -PropertyType MultiString -Force
 }
 
 
-Function Sort-GpxFile($inputFileName, $outputFileName)
+Function Sort-GpxFile($InputFileName, $OutputFileName)
 {
     # See also: https://personalnexus.wordpress.com/2018/09/26/the-case-of-lightroom-placing-all-photos-at-the-end-of-the-track-sorting-a-gpx-file/
 
-    [xml]$xml = Get-Content $inputFileName
+    [xml]$xml = Get-Content $InputFileName
     ($points = ($xml.gpx.trk.trkseg.trkpt | Sort-Object -Property time -Descending) ) | Out-Null
     $firstPoint = $points[-1]
     $points | ForEach-Object { $xml.gpx.trk.trkseg.InsertAfter($_, $firstPoint) } | Out-Null
-    $xml.Save($outputFileName)
+    $xml.Save($OutputFileName)
+}
+
+
+Function Convert-SidToUsername($SidString)
+{
+    # See also: https://personalnexus.wordpress.com/2018/10/06/the-case-of-low-disk-space-because-of-another-users-recycle-bin/
+
+    $sid = New-Object System.Security.Principal.SecurityIdentifier($SidString)
+    
+    try
+    {
+       $user = $sid.Translate([System.Security.Principal.NTAccount])
+       $result = $user.Value
+    }
+    catch
+    {
+        $result = "Unknown user: $SidString"
+    }
+    return $result
+}
+
+
+Function Convert-UsernameToSid($Domain = "", $Username)
+{
+    # See also: https://personalnexus.wordpress.com/2018/10/06/the-case-of-low-disk-space-because-of-another-users-recycle-bin/
+
+    if ($Domain)
+    {
+        $user = New-Object System.Security.Principal.NTAccount($Domain, $Username)
+    }
+    else
+    {
+        $user = New-Object System.Security.Principal.NTAccount($Username)
+    }
+
+    try
+    {
+        $sid = $user.Translate([System.Security.Principal.SecurityIdentifier])
+        $result = $sid.Value
+    }
+    catch
+    {
+        $result = "Unknown user: $Domain\$Username"
+    }
+    return $result
 }
